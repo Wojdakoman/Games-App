f53702d0b9045257600c09d70434b714356a88c7
Daniel Wojdak <logiblogi@gmail.com>
2021-04-26 15:31:33 +0200

    SignUp fragment

--- app/src/main/java/com/example/gameapp/views/loginFragments/SignUpFragment.kt
+++ app/src/main/java/com/example/gameapp/views/loginFragments/SignUpFragment.kt
@@ -1,21 +1,23 @@
 package com.example.gameapp.views.loginFragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.navigation.findNavController
 import com.example.gameapp.R
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
-import com.google.firebase.auth.FirebaseAuthInvalidUserException
+import com.google.firebase.auth.*
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_sign_up.*
+import kotlinx.android.synthetic.main.fragment_sign_up.btnSignUp
+import kotlinx.android.synthetic.main.fragment_sign_up.txtUserLogin
+import kotlinx.android.synthetic.main.fragment_sign_up.txtUserPass
 
-class LoginFragment : Fragment() {
+class SignUpFragment : Fragment() {
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,15 +31,17 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //sign in button
-        btnLogin.setOnClickListener {
+
+        //sign up button
+        btnSignUp.setOnClickListener {
             var login: String = txtUserLogin.text.toString()
             var pass: String = txtUserPass.text.toString()
+            var pass2: String = txtUserPass2.text.toString()
 
             var canProceed = true
 
@@ -53,23 +57,30 @@ class LoginFragment : Fragment() {
             }
             else txtUserLogin.error = null
 
-            if(canProceed) login(login, pass)
-        }
-        //sign up button
-        btnSignUp.setOnClickListener {
-            view.findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+            if(pass2.isNullOrBlank()){
+                txtUserPass2.error = getString(R.string.inputBlank)
+                canProceed = false
+            } else if(pass != pass2){
+                txtUserPass2.error = getString(R.string.passwordDiffer)
+                canProceed = false
+            }
+            else txtUserLogin.error = null
+
+            if(canProceed) signUp(login, pass)
         }
     }
 
-    private fun login(login: String, pass: String){
-        auth.signInWithEmailAndPassword(login, pass).addOnCompleteListener(requireActivity()) {
+    private fun signUp(login: String, password: String){
+        auth.createUserWithEmailAndPassword(login, password).addOnCompleteListener(requireActivity()){
             if(it.isSuccessful){
-                Toast.makeText(context, "Zalogowano!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "Zarejestrowano!", Toast.LENGTH_SHORT).show()
             } else {
+                Log.d("[LOG]", "er: ${it.exception}")
                 var msg = ""
                 when(it.exception){
-                    is FirebaseAuthInvalidCredentialsException -> msg = "Podano nieprawidłowy login"
-                    is FirebaseAuthInvalidUserException -> msg = "Brak użytkownika"
+                    is FirebaseAuthWeakPasswordException-> msg = "Wybrane hasło jest za słabe"
+                    is FirebaseAuthInvalidCredentialsException -> msg = "Podaj email jako login"
+                    is FirebaseAuthUserCollisionException -> msg = "Konto już istnieje!"
                     else -> msg = "unknown"
                 }
                 Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
