7aa01251060a0764b57dcecd5ee268d833a5259f
Daniel Wojdak <logiblogi@gmail.com>
2021-04-26 12:47:16 +0200

    Initialize

---
+++ app/src/main/java/com/example/gameapp/views/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gameapp.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gameapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
