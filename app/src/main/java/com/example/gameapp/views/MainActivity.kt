db01c24127aac6f83a02ae4241106d0dea8a0a1e
Daniel Wojdak <logiblogi@gmail.com>
2021-04-26 13:21:25 +0200

    Add Login fragment

--- app/src/main/java/com/example/gameapp/views/MainActivity.kt
+++ app/src/main/java/com/example/gameapp/views/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.gameapp.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.gameapp.R
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var auth = Firebase.auth
+        if(auth.currentUser == null){
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
         setContentView(R.layout.activity_main)
     }
 }
