7ff57b8b596671f5f01e078365d70dba933a0132
Daniel Wojdak <logiblogi@gmail.com>
2021-05-10 19:15:22 +0200

    Save token in memory

--- app/src/main/java/com/example/gameapp/viewmodels/GamesViewModel.kt
+++ app/src/main/java/com/example/gameapp/viewmodels/GamesViewModel.kt
@@ -3,6 +3,7 @@ package com.example.gameapp.viewmodels
 import android.app.Application
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.gameapp.models.GamesRepository
 import com.example.gameapp.models.api.GamesAPI
@@ -10,11 +11,20 @@ import kotlinx.coroutines.launch
 
 class GamesViewModel(application: Application): AndroidViewModel(application) {
     private val repository = GamesRepository(GamesAPI())
+    var token = MutableLiveData<String>()
 
-     fun loadAccessToken(){
-        viewModelScope.launch {
-            repository.ACCESS_TOKEN = repository.getAccessToken().access_token
-            Log.d("[TOKEN]", "tplem: ${repository.ACCESS_TOKEN}")
-        }
+     fun loadAccessToken(access_token: String?){
+         Log.d("[TOKEN_SAVED]", "ts: $access_token")
+         if(access_token.isNullOrEmpty()){
+             viewModelScope.launch {
+                 repository.ACCESS_TOKEN = repository.getAccessToken().access_token
+                 token.postValue(repository.ACCESS_TOKEN)
+                 Log.d("[TOKEN]", "token got: ${repository.ACCESS_TOKEN}")
+             }
+         } else {
+             repository.ACCESS_TOKEN = access_token
+             Log.d("[TOKEN]", "token saved: ${repository.ACCESS_TOKEN}")
+         }
+
     }
 }
--- app/src/main/java/com/example/gameapp/views/MainFragment.kt
+++ app/src/main/java/com/example/gameapp/views/MainFragment.kt
@@ -1,11 +1,15 @@
 package com.example.gameapp.views
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.gameapp.R
 import com.example.gameapp.viewmodels.GamesViewModel
@@ -17,6 +21,7 @@ import kotlinx.android.synthetic.main.fragment_main.*
 class MainFragment : Fragment() {
     private lateinit var viewModel: GamesViewModel
     private lateinit var auth: FirebaseAuth
+    private lateinit var preferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +37,20 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         viewModel = ViewModelProvider(this).get(GamesViewModel::class.java)
-        viewModel.loadAccessToken()
+        preferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
+
+        viewModel.loadAccessToken(preferences?.getString("access_token", ""))
+
+        viewModel.token.observe(viewLifecycleOwner, Observer {
+            if(preferences != null){
+                Log.d("[SAVE]", "token: $it")
+                with(preferences.edit()){
+                    putString("access_token", it)
+                    apply()
+                }
+            }
+        })
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
