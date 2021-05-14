66a87f14b2bf44b519c24292d15796fa095a1ba1
Daniel Wojdak <logiblogi@gmail.com>
2021-05-14 21:16:03 +0200

    Bottom navigation step 1

--- app/src/main/java/com/example/gameapp/views/MainActivity.kt
+++ app/src/main/java/com/example/gameapp/views/MainActivity.kt
@@ -3,14 +3,34 @@ package com.example.gameapp.views
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.gameapp.R
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.fragment)
+        bottom_navigation.setupWithNavController(navController)
+
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
     }
 }
--- app/src/main/java/com/example/gameapp/views/MainFragment.kt
+++ app/src/main/java/com/example/gameapp/views/MainFragment.kt
@@ -16,6 +16,7 @@ import com.example.gameapp.viewmodels.GamesViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
